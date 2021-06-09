853102634dbc6c8e26bfd46a9302d434aa7ccbd2
Amnah Ali <amnah1li@gmail.com>
2021-06-09 09:47:35 +0300

    update the way of show fragment

--- app/src/main/java/com/cotton/candy/todo/ui/MainActivity.kt
+++ app/src/main/java/com/cotton/candy/todo/ui/MainActivity.kt
@@ -2,8 +2,10 @@ package com.cotton.candy.todo.ui
 
 import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.cotton.candy.todo.R
 import com.cotton.candy.todo.databinding.ActivityMainBinding
+
 import com.cotton.candy.todo.fragment.TaskFragment
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -22,9 +24,19 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         }
     }
 
+    //load fragment to enter tasks with simple animation
     private fun loadFragments(fragment: Fragment){
-        supportFragmentManager.beginTransaction().apply {
+        val transaction =  supportFragmentManager.beginTransaction()
+        transaction?.apply {
+            setCustomAnimations(
+                R.anim.slide_from_right,
+                R.anim.slideout_from_left,
+                R.anim.slide_from_left,
+                R.anim.slideout_from_right
+            )
             add(R.id.fragment_container, fragment)
+            addToBackStack(java.lang.String.valueOf(this))
+            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             commit()
         }
     }
